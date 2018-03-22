cf9b10e6eed11d66b9732f75c242eb9bcb833051
vlad <vlad.semenchuk97@gmail.com>
2018-03-22 14:27:19 +0200

    ready header

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 $(function(){
-	var navBg = $('#menu');
+	var navBg = $('#menu'),
+		logo = $('.logo a'),
+		menuList = $('.navlist li a ');
+
 	$(window).scroll(function(){
 		if($(this).scrollTop() > 0){
-			navBg.css({
-				'background' : '#fff',
+			navBg.css('background', '#fff');
+			logo.css({
+				'color' : "#17baef",
+				'transition' : 'all .4s'
 			});
-		};
+			menuList.css('color', '#333');
+		} else {
+			navBg.css('background', 'transparent');
+			logo.css({
+				'color' : "#fff",
+				'transition' : 'all .4s'
+			});
+			menuList.css('color', '#eee');
+		}
 	});
 });
